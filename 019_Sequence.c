1a52f3364a92d183127a64224d0d1653f7a5504d
sumanthkumarkoti <pallisumanthkumarkoti@gmail.com>
2024-06-15 10:50:17 +0530

    Add files via upload

---
+++ 019_Sequence.c
@@ -0,0 +1,16 @@
+// Program to display given matter in the Center of the screen
+
+# include <stdio.h>
+
+main()
+
+{
+    printf("\n\t\t\t\t\t\tAH CAREER PVT LTD");
+    printf("\n");
+    printf("\n\t\t\t\t\t\t   Danvaipeth");
+    printf("\n");
+    printf("\n\t\t\t\t\t\t   Rajahmundry.");
+    printf("\n");
+
+    getch();
+}
