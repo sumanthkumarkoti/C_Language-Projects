bf4be6c08f8e132534da8dd2e46994bf83428238
sumanthkumarkoti <pallisumanthkumarkoti@gmail.com>
2024-06-15 10:37:29 +0530

    Add files via upload

---
+++ 001_Sequence.c
@@ -0,0 +1,17 @@
+/*
+Program Name: 001(Sequence).c
+Objective: Displays Personal Information
+Author: Sumanth
+Date: 6th June 2024
+*/
+# include <stdio.h>
+main()
+{
+    printf("\n Name: P.Sumanth Kumar Koti ");
+    printf("\n");
+    printf("\n DOB: May 19, 2007 ");
+    printf("\n");
+    printf("\n Moblie: +91-9848239269");
+    printf("\n");
+}
+
