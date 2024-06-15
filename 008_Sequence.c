efae1702c5de1b98c92223c2eb4aa37708fe6528
sumanthkumarkoti <pallisumanthkumarkoti@gmail.com>
2024-06-15 10:42:53 +0530

    Add files via upload

---
+++ 008_Sequence.c
@@ -0,0 +1,33 @@
+//Program to read days and convert it into years,months,weeks and days\
+
+# include <stdio.h>
+
+int main()
+
+{
+
+    int i,y,m,w,d;
+
+    i=y=m=w=d=0;
+
+    printf("\n");
+    printf(" Enter No of days: ");
+    scanf("%d",&i);
+
+    y = i / 365;
+
+
+    printf("\n");
+    printf(" Years: %d",y);
+    printf("\n");
+    printf(" Months: %d",m);
+    printf("\n");
+    printf(" Weeks: %d",w);
+    printf("\n");
+    printf(" Days: %d",d);
+    printf("\n");
+
+
+    getch();
+
+}
